11e7fc67114316a9b4b0de2df37982bf47abad91
lsevde <lsevde@outlook.com>
2015-11-22 18:24:47 -0700

    I updated the fillArrays and fillArray functions. The update from earlier stored random numbers but both arrays were different. Now both arrays have the same random numbers, I just don’t know what to do in the fillArray function or with the variable bool resp. Let me know what you think!

--- Sevde-assn4-funcs.cpp
+++ Sevde-assn4-funcs.cpp
@@ -1,6 +1,7 @@
 #include "Sevde-assn4-funcs.h"
 
 #include <iostream>
+#include <iomanip>
 #include <ctime>
 #include <string>
 
@@ -43,16 +44,21 @@ void  dellocArray(int array[]) {
 //  IMPLEMENTED BY: Lindsay
 //**************************************************************************
 bool  fillArrays(int array1[],int array2[]) { // will call srand once
+   
     bool resp = false;
+    int tempVariable1;              //temporary storage for random integer
+    int tempVariable2;
     
-    if (resp == false) {
-        resp = fillArray(array1);
-        if (resp == true) {
-            resp = fillArray(array2);
-        }
-        else {
-            resp = false;
-        }
+    //Generates random 100,000 random numbers between 1-30,000 and stores it
+    for (int countNums1 = 0; countNums1 < ARRAY_SIZE; countNums1++) {
+        tempVariable1 = rand() % END_RANGE + 1;
+        array1[countNums1] = tempVariable1;
+        cout << array1[countNums1] << endl;
+    }
+    
+    for (int countNums2 = 0; countNums2 < ARRAY_SIZE; countNums2++) {
+        tempVariable2 = array1[countNums2];
+        array2[countNums2] = tempVariable2;
     }
     
   return resp;	
@@ -68,23 +74,8 @@ bool  fillArrays(int array1[],int array2[]) { // will call srand once
 bool  fillArray(int array[]) {
 	
     bool resp;
-    int tempVariable;              //temporary storage for random integer
     
-    // first two statements of function
-    int startTime, endTime, elapsedTime;
-    startTime = clock();
-    
-    //Generates random 100,000 random numbers between 1-30,000 and stores it
-    for (int countNums = 0; countNums < ARRAY_SIZE; countNums++) {
-        tempVariable = rand() % END_RANGE + 1;
-        array[countNums] = tempVariable;
-    }
-    
-    
-    // last three statements
-    endTime = clock();
-    elapsedTime = endTime - startTime;
-    cout << "Bubble sort time " << elapsedTime << endl;
+
     
 	return resp;
 }
@@ -140,6 +131,46 @@ time_t  sortArrayQuick(int array[]) {
 //**************************************************************************
 void    displayResults(Menu sortType1, time_t sortTime1[], Menu sortType2, time_t sortTime2[]) {
 	
-	
+    cout << "---------------" << endl;
+    cout << fixed << setprecision(1) << showpoint;
+    
+    //CHANGE THESE once we have the string Labels working
+    if (sortType1 == 0) {
+        cout << setw(21) << "Boubble Sort " << sortTime1
+             << " clock ticks on average" << endl;
+    }
+    else if (sortType1 == 1) {
+        cout << setw(21) << "Insertion Sort " << sortTime1
+             << " clock ticks on average"<< endl;
+    }
+    else if (sortType1 == 2) {
+        cout << setw(21) << "Merge Sort " << sortTime1
+             << " clock ticks on average" << endl;
+    }
+    else if (sortType1 == 3) {
+        cout << setw(21) << "Quick Sort " << sortTime1
+             << " clock ticks on average" << endl;
+    }
+    
+    
+    if (sortType2 == 0) {
+        cout << setw(21) << "Boubble Sort " << sortTime2
+        << " clock ticks on average" << endl;
+    }
+    else if (sortType2 == 1) {
+        cout << setw(21) << "Insertion Sort " << sortTime2
+        << " clock ticks on average"<< endl;
+    }
+    else if (sortType2 == 2) {
+        cout << setw(21) << "Merge Sort " << sortTime2
+        << " clock ticks on average" << endl;
+    }
+    else if (sortType2 == 3) {
+        cout << setw(21) << "Quick Sort " << sortTime2
+        << " clock ticks on average" << endl;
+    }
+    
+    
+    cout << endl << "Press any key to continue . . ." << endl;
 }
 
